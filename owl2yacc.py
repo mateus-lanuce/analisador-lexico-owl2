7654cdad68839d5f7a6c5edd6af24932a9e5d23d
mateus_lanuce <mateuslanuce@gmail.com>
2025-01-09 12:22:41 -0300

    feat: enhance class declaration and subclass expression handling with improved logging and error reporting

--- owl2yacc.py
+++ owl2yacc.py
@@ -16,15 +16,22 @@ precedence = (
 
 def p_class_declaration(p):
     """class_declaration : CLASS IDENTIFIER class_body"""
-    p[0] = ('ClassDeclaration', p[2], p[3])
+    p[0] = ('ClassDeclaration', p[1], p[2], p[3])
+
+    if p[3][0] == 'ClassBodyPrimitive':
+        print('\nclasse primitiva declarada: \n', p[1], p[2], p[3])
+    elif p[3][0] == 'ClassBodyPrimitiveClosure':
+        print('\nclasse primitiva com axioma de fechamento declarada: \n', p[1], p[2], p[3])
 
 def p_class_body(p):
     """class_body : primitive_body
                     | defined_body
-                    | nested_body
-                    | closure_body
                   """
-    p[0] = ('ClassBody', p[1:])
+    
+    if p[1][0] == 'primitiveBody':
+        p[0] = ('ClassBodyPrimitive', p[1:])
+    elif p[1][0] == 'primitiveBodyClosure':
+        p[0] = ('ClassBodyPrimitiveClosure', p[1:])
 
 def p_class_body_error(p):
     """class_body : error"""
@@ -36,8 +43,11 @@ def p_primitive_body(p):
                    | subclass_section individuals_section
                    | subclass_section
                       """
-    print('classe primitiva: ', p[1:])
-    p[0] = ('ClassBody', p[1])
+
+    if p[1][0] == 'SubClassOf':
+        p[0] = ('primitiveBody', p[1])
+    elif p[1][0] == 'SubClassOfClosure':
+        p[0] = ('primitiveBodyClosure', p[1])
 
 #classe definida
 def p_defined_body(p):
@@ -49,41 +59,34 @@ def p_defined_body(p):
     print('classe definida: ', p[1:])
     p[0] = ('ClassBody', p[1])
 
-#corpo de Classe com axioma de fechamento (closure axiom)
-def p_closure_body(p):
-    """closure_body : subclass_section_only disjoint_section individuals_section
-                     | subclass_section_only disjoint_section
-                     | subclass_section_only individuals_section
-                     | subclass_section_only
-                      """
-    print('classe com axioma de fechamento: ', p[1:])
-    p[0] = ('ClassBody', p[1])
-
-#corpo de classe com  Classe com descrições aninhadas
-def p_nested_body(p):
-    """nested_body : equivalent_section_nested disjoint_section individuals_section
-                     | equivalent_section_nested disjoint_section
-                     | equivalent_section_nested individuals_section
-                     | equivalent_section_nested
-                      """
-    print('classe com descrições aninhadas: ', p[1:])
-    p[0] = ('ClassBody', p[1])
-
 #TODO: equivalent_to
 
 def p_subclass_section(p):
     """subclass_section : SUBCLASSOF subclass_expressions"""
-    p[0] = ('SubClassOf', p[2:])
+    # se tiver uma subclassExpressionClosure, modificar para SubClassOfClosure, necessario percorrer a lista de subclass_expressions
+    for i in p[2]:
+        if i[0] == 'SubClassExpressionClosure':
+            p[0] = ('SubClassOfClosure', *p[2:])
+            return
+
+    p[0] = ('SubClassOf', *p[2:])
+
+def p_subclass_section_error(p):
+    """subclass_section : error"""
+    print('Erro de sintaxe na declaração de subclasse')
     
 def p_subclass_expressions(p):
     """subclass_expressions : subclass_expressions SPECIAL subclass_expression
                              | subclass_expression
-                             | subclass_expressions_only
                              """
     if len(p) == 2:
         p[0] = [p[1]]
     else:
         p[0] = p[1] + [p[3]]
+
+def p_subclass_expressions_error(p):
+    """subclass_expressions : error"""
+    print('Erro de sintaxe na declaração de expressão de subclasse')
 
 def p_subclass_expression(p):
     """subclass_expression : PROPERTY SOME IDENTIFIER
@@ -93,68 +96,59 @@ def p_subclass_expression(p):
                             | PROPERTY MAX INTEGER
                             | PROPERTY EXACTLY INTEGER
                             | PROPERTY SOME TYPE
-                            | PROPERTY SOME '(' PROPERTY VALUE TYPE ')'
-                            | identifier_list
+                            | PROPERTY SOME SPECIAL PROPERTY VALUE TYPE SPECIAL
+                            | IDENTIFIER
                             | PROPERTY ONLY SPECIAL IDENTIFIER OR IDENTIFIER SPECIAL
                             """
     if len(p) == 2:
         print ('analisado uma subclass_expression', p[1])
         p[0] = ('SubClassExpression', p[1])
+    elif len(p) == 8:
+        print('analisado uma subclass_expression com axioma de fechamento', p[1], p[2], p[3], p[4], p[5], p[6])
+        p[0] = ('SubClassExpressionClosure', p[1], p[2], p[3], p[4], p[5], p[6])
     else:
         print('parsed a subclass expression', p[1], p[2], p[3])
-        p[0] = ('SubClassExpression', p[1], p[2:])    
-    
-def p_subclass_section_only(p):
-    """subclass_section_only : SUBCLASSOF subclass_expressions_only"""
-    p[0] = ('SubClassOf', p[2])
+        p[0] = ('SubClassExpression', p[1], p[2:])   
 
-def p_subclass_expressions_only(p):
-    """subclass_expressions_only : subclass_expressions_only SPECIAL subclass_expression_only
-                             | subclass_expression_only"""
-    if len(p) == 2:
-        p[0] = [p[1]]
-    else:
-        p[0] = p[1] + [p[3]]
-
-def p_subclass_expression_only(p):
-    """subclass_expression_only : PROPERTY ONLY SPECIAL IDENTIFIER OR IDENTIFIER SPECIAL"""
-    print('parsed a subclass expression only', p[1], p[2], p[3])
-    p[0] = ('SubClassExpression', p[1], p[2:])
+def p_subclass_expression_error(p):
+    """subclass_expression : error"""
+    print('Erro de sintaxe na declaração de subclasse') 
 
 def p_disjoint_section(p):
     """disjoint_section : DISJOINTCLASSES identifier_list"""
-    p[0] = ('DisjointClasses', p[3])
+    p[0] = ('DisjointClasses', p[2])
 
 def p_identifier_list(p):
-    """identifier_list : identifier_list ',' IDENTIFIER
+    """identifier_list : identifier_list SPECIAL IDENTIFIER
                         | IDENTIFIER"""
     if len(p) == 2:
-        p[0] = [p[1]]
+        p[0] = p[1]
     else:
-        p[0] = p[1] + [p[3]]
+        p[0] = list()
+        p[0].append(p[1])
+        if type(p[1]) == list:
+            p[1].append(p[3])
+            p[0].append(p[1])
+        else:
+            p[0].append(p[3])
 
 def p_individuals_section(p):
     """individuals_section : INDIVIDUALS individual_list"""
-    p[0] = ('Individuals', p[3])
+    p[0] = ('Individuals', p[2:])
 
 def p_individual_list(p):
-    """individual_list : individual_list ',' INDIVIDUAL
+    """individual_list : individual_list SPECIAL INDIVIDUAL
                         | INDIVIDUAL"""
     if len(p) == 2:
-        p[0] = [p[1]]
+        p[0] = p[1]
     else:
-        p[0] = p[1] + [p[3]]
+        p[0] = list()
+        p[0].append(p[1])
+        p[0].append(p[3])
     
 def p_equivalent_section(p): 
     """equivalent_section :  EQUIVALENTO identifier_list AND subclass_expression """
     p[0] = p[1] + p[3]
-
-def p_equivalent_section_nested(p):
-    """equivalent_section_nested : equivalent_section AND subclass_expression
-                                  | equivalent_section AND identifier_list SOME subclass_expression"""
-    p[0] = p[1] + p[3]
-
-#na equivalent pode ter, identificador, lista de subclassExpressions
 
 def p_error(p):
     print("Erro de sintaxe em '%s'" % p.value if p else "EOF")
@@ -163,12 +157,15 @@ def p_error(p):
 # Construtor do parser
 parser = yacc.yacc()
 
-dataTeste = '''Class: MargheritaPizza
-SubClassOf:
-NamedPizza,
-hasTopping some MozzarellaTopping,
-hasTopping some TomatoTopping,
-hasTopping only (MozzarellaTopping or TomatoTopping)
+dataTeste = ''' Class: Pizza
+ SubClassOf:
+ hasBase some PizzaBase,
+ hasCaloricContent some xsd:integer
+ DisjointClasses:
+ Pizza, PizzaBase, PizzaTopping
+ Individuals:
+ CustomPizza1,
+ CustomPizza2
 '''
 
 lexer.input(dataTeste)
