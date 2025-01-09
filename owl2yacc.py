78f694c85a33828497ef4c43217df71354588790
mateus_lanuce <mateuslanuce@gmail.com>
2025-01-09 12:40:56 -0300

    feat: add error handling for class body, disjoint section, individuals section, and identifier list

--- owl2yacc.py
+++ owl2yacc.py
@@ -33,6 +33,8 @@ def p_class_body(p):
     elif p[1][0] == 'primitiveBodyClosure':
         p[0] = ('ClassBodyPrimitiveClosure', p[1:])
 
+    #TODO: adicionar regras para defined_body
+
 def p_class_body_error(p):
     """class_body : error"""
     print('Erro de sintaxe na declaração de classe')
@@ -118,6 +120,10 @@ def p_disjoint_section(p):
     """disjoint_section : DISJOINTCLASSES identifier_list"""
     p[0] = ('DisjointClasses', p[2])
 
+def p_disjoint_section_error(p):
+    """disjoint_section : error"""
+    print('Erro de sintaxe na declaração de disjoint classes')
+
 def p_identifier_list(p):
     """identifier_list : identifier_list SPECIAL IDENTIFIER
                         | IDENTIFIER"""
@@ -132,9 +138,17 @@ def p_identifier_list(p):
         else:
             p[0].append(p[3])
 
+def p_identifier_list_error(p):
+    """identifier_list : error"""
+    print('Erro de sintaxe na declaração de lista de identificadores')
+
 def p_individuals_section(p):
     """individuals_section : INDIVIDUALS individual_list"""
     p[0] = ('Individuals', p[2:])
+
+def p_individuals_section_error(p):
+    """individuals_section : error"""
+    print('Erro de sintaxe na declaração de individuos')
 
 def p_individual_list(p):
     """individual_list : individual_list SPECIAL INDIVIDUAL
@@ -145,10 +159,37 @@ def p_individual_list(p):
         p[0] = list()
         p[0].append(p[1])
         p[0].append(p[3])
+
+def p_individual_list_error(p):
+    """individual_list : error"""
+    print('Erro de sintaxe na declaração de lista de individuos')
     
 def p_equivalent_section(p): 
-    """equivalent_section :  EQUIVALENTO identifier_list AND subclass_expression """
-    p[0] = p[1] + p[3]
+    """equivalent_section :  EQUIVALENTO equivalent_expressions"""
+    p[0] = p[2]
+
+def p_equivalent_expressions(p):
+    """equivalent_expressions : equivalent_expressions SPECIAL equivalent_expression
+                                | equivalent_expression
+                                """
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[3]]
+
+def p_equivalent_expression(p):
+    """equivalent_expression : identifier_list
+                               | IDENTIFIER AND SPECIAL complex_property_expression SPECIAL
+                               """
+    p[0] = ('EquivalentExpression', p[1:])
+
+def p_complex_property_expression_equivalent_to(p):
+    """complex_property_expression : PROPERTY SOME IDENTIFIER
+                                      | PROPERTY SOME SPECIAL PROPERTY VALUE TYPE SPECIAL
+                                      """
+    p[0] = ('ComplexPropertyExpression', p[1:])
+
+
 
 def p_error(p):
     print("Erro de sintaxe em '%s'" % p.value if p else "EOF")
@@ -157,15 +198,11 @@ def p_error(p):
 # Construtor do parser
 parser = yacc.yacc()
 
-dataTeste = ''' Class: Pizza
- SubClassOf:
- hasBase some PizzaBase,
- hasCaloricContent some xsd:integer
- DisjointClasses:
- Pizza, PizzaBase, PizzaTopping
+dataTeste = '''Class: CheesyPizza
+ EquivalentTo:
+ Pizza and (hasTopping some CheeseTopping)
  Individuals:
- CustomPizza1,
- CustomPizza2
+ CheesyPizza1
 '''
 
 lexer.input(dataTeste)
